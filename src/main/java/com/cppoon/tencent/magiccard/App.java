668e02eaa3e827266c47e409ee9fe23aaa26c95c
Cyril Poon <cyril.poon@museviral.com>
2014-01-03 13:16:10 +0800

    * initial commit.

---
+++ src/main/java/com/cppoon/tencent/magiccard/App.java
@@ -0,0 +1,13 @@
+package com.cppoon.tencent.magiccard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
