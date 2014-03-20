ac44dc0354b6bde3d0e591918e988560ae03e701
Cyril Poon <cyril.poon@museviral.com>
2014-03-20 18:34:34 +0800

    * code cleanup.

--- src/main/java/com/cppoon/tencent/magiccard/vendor/qzapp/parser/impl/StolenStoveParser20140319.java
+++ src/main/java/com/cppoon/tencent/magiccard/vendor/qzapp/parser/impl/StolenStoveParser20140319.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 
 import com.cppoon.tencent.magiccard.api.StoveStatus;
 import com.cppoon.tencent.magiccard.vendor.qzapp.parser.StolenStove;
-import com.cppoon.tencent.magiccard.vendor.qzapp.parser.StoveInfo;
 
 /**
  * 
@@ -168,81 +167,6 @@ public class StolenStoveParser20140319 {
 		return (html.contains("红钻特权") || html.contains("开通红钻")); 
 	}
 	
-	public List<StolenStove> parse_old(String html) {
-
-		ArrayList<StolenStove> ret = new ArrayList<StolenStove>();
-
-		Matcher m = getStovePattern().matcher(html);
-		while (m.find()) {
-			
-			String stoveHtml = m.group();
-			log.trace("group: [[{}]]", stoveHtml);
-			
-			// we don't handle unopened stoves
-			if (stoveHtml.contains("红钻特权") || stoveHtml.contains("开通红钻")) {
-				continue;
-			}
-			
-			StolenStove si = parseStove(stoveHtml);
-			
-			if (si == null) {
-				log.warn("an error occurred when parsing stove");
-				return null;
-			}
-			
-			ret.add(si);
-		}
-
-		return ret;
-		
-	}
-	
-	/**
-	 * Extract stove information.
-	 */
-	protected StolenStove parseStove(String html) {
-		
-		StolenStove ret = new StolenStove();
-		
-		if (!parseStoveStatus(ret, html)) {
-			return null;
-		}
-		
-		switch (ret.getStatus()) {
-		case IDLE:
-			break;
-		case SYNTHESIZED:
-			if (!parseCardInfo(ret, html)) return null;
-			break;
-		case SYNTHESIZING:
-			if (!parseCardInfo(ret, html)) return null;
-			break;
-		default:
-			log.warn("unknonw stove status {}", ret.getStatus());
-			return null;
-		}
-		
-		return ret;
-		
-	}
-	
-	protected boolean parseStoveStatus(StolenStove ret, String html) {
-		
-		if (html.contains("已合成")) {
-			ret.setStatus(StoveStatus.SYNTHESIZED);
-			return true;
-		} else if (html.contains("空闲中")) {
-			ret.setStatus(StoveStatus.IDLE);
-			return true;
-		} else if (html.contains("合成中")) {
-			ret.setStatus(StoveStatus.SYNTHESIZING);
-			return true;
-		}
-		
-		return false;
-		
-	}
-	
 	protected StoveStatus parseStoveStatus(String html) {
 		
 		if (html.contains("已合成")) {
@@ -254,33 +178,6 @@ public class StolenStoveParser20140319 {
 		}
 		
 		return null;
-		
-	}
-	
-	
-	protected boolean parseCardInfo(StolenStove ret, String html) {
-
-		Matcher m = getCardInfoPattern().matcher(html);
-		
-		if (!m.find()) {
-			log.warn("unable to extract card info in stolen stove");
-			return false;
-		}
-		
-		String cardThemeName = StringUtils.trim(m.group(1));
-		String cardName = StringUtils.trim(m.group(2));
-		String sCardPrice = StringUtils.trim(m.group(3));
-		
-		
-		ret.setCardThemeName(cardThemeName);
-		ret.setCardName(cardName);
-		try {
-			ret.setCardPrice(Double.parseDouble(sCardPrice));
-		} catch (NumberFormatException e) {
-			log.warn("error parsing card price", e);
-		}
-		
-		return true;
 		
 	}
 	
