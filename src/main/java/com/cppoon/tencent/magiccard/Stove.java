597d3de6f6c472bce09e8674d18ed398b5b9c267
Cyril Poon <cyril.poon@museviral.com>
2014-04-03 19:24:58 +0800

    * added Stove interface.

--- src/main/java/com/cppoon/tencent/magiccard/Stove.java
+++ src/main/java/com/cppoon/tencent/magiccard/Stove.java
@@ -12,13 +12,39 @@ import com.cppoon.tencent.magiccard.api.StoveStatus;
  * @since 0.1.0
  */
 public interface Stove {
-	
+
+	/**
+	 * Cancel any card synthesis on this stove.
+	 */
 	void cancel();
-	
+
+	/**
+	 * Checks whether the card synthesis can be cancelled.
+	 * 
+	 * @return
+	 */
+	boolean isCancellable();
+
+	/**
+	 * Synthesize the specified card.
+	 * 
+	 * @param card
+	 */
 	void synthesize(Card card);
-	
+
+	/**
+	 * Returns the status of this stove.
+	 * 
+	 * @return
+	 */
 	StoveStatus getStatus();
-	
-	void getCard();
+
+	/**
+	 * Returns the card in this stove, if any.
+	 * <p>
+	 * 
+	 * If there is no card in the stove, this method returns <code>null</code>.
+	 */
+	Card getCard();
 
 }
